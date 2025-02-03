ea8073e11f6a4cf439b29c7b3bbb53e5678e678b
Tara <b2028tlspablo@pshs.edu.ph>
2025-02-03 20:41:30 +0800

    updated the cutscene

--- cutsceneTest.js
+++ cutsceneTest.js
@@ -1,6 +1,6 @@
 function createCutscene() {
   document.getElementById("container").style.display = "none";
   const myWindow = window.open();
-  myWindow.document.write( '<link href="cutscene.css" rel="stylesheet" type="text/css"/>, <div class="cutscene"> <video width="window.innerWidth" height="window.innerHeight" autoplay muted> <source src="https://cdn.glitch.global/9c48f61b-bf4f-455f-a449-a1e4225c79a2/cutsceneTest.mp4?v=1737288208619" type="video/mp4"> </video> </div>'  
+  myWindow.document.write( '<link href="cutscene.css" rel="stylesheet" type="text/css"/>, <div class="cutscene"> <video width="window.innerWidth" height="window.innerHeight" autoplay muted> <source src="images/eidolonanimatic.mp4" type="video/mp4"> </video> </div>'  
   );
 }
